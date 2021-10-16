326beabe3395b43071aaffbec536ba048aded819
kxrxll <kxrxll@kxrxlls-MacBook-Air.local>
2021-10-16 20:39:20 +0300

    structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# Dom task number one [![Build status](https://ci.appveyor.com/api/projects/status/yhfoar8fm8puu1vb?svg=true)](https://ci.appveyor.com/project/kxrxll/ahj-dom1)
---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import GoblinGame from './GoblinGame';
+
+const goblinGame = new GoblinGame(document.querySelector('body'));
+goblinGame.drawTable();
+goblinGame.startGame();
+goblinGame.initGame();
